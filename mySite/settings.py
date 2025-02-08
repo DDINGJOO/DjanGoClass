504eda36f42ac12d50744cb7f6952e694d7980c4
DDINGJOO <ddingsha9@teambind.co.kr>
2025-02-08 14:54:43 +0900

    FEAT : 2025-02-08 CLASS DATA -Final-

--- mySite/settings.py
+++ mySite/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'mySite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'mySite/templates')],  # ✅ 여기에 추가!
+        'DIRS': [os.path.join(BASE_DIR, 'mySite/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
